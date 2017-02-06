aa20d13babb01de0f53f08681ef0ac6b838893e9
Raphael Amorim <rapha850@gmail.com>
2017-02-06 01:19:52 -0200

    testing ast

--- index.js
+++ index.js
@@ -5,17 +5,73 @@ const requireFromString = require('require-from-string')
 const reactDOMServer = require('react-dom/server')
 
 function Masamune(componentPath, template) {
+  function getRequires(requires, body) {
+    return new Promise((resolve) => {
+    for (var i = 0; i < body.length; i++) {
+      if (!body[i] || !body[i].type) return
+
+      if (body[i].type === 'VariableDeclarator') {
+        getRequires(requires, [body[i].init])
+      }
+      if (body[i].type === 'VariableDeclaration') {
+        getRequires(requires, body[i].declarations)
+      }
+      if (body[i].type === 'ConditionalExpression') {
+        getRequires(requires, [body[i].test, body[i].consequent])
+      }
+      if (body[i].type === 'AssignmentExpression') {
+        getRequires(requires, [body[i].left, body[i].right])
+      }
+      if (body[i].type === 'ExpressionStatement') {
+        if (body[i].expression.type === 'SequenceExpression')
+          getRequires(requires, body[i].expression.expressions)
+
+        getRequires(requires, [body[i].expression.left, body[i].expression.right])
+      }
+      if (body[i].type === 'FunctionDeclaration') {
+        getRequires(requires, (body[i].body.body || []))
+      }
+      if (body[i].type === 'ImportDeclaration') {
+        requires.push(body[i].source.value)
+      }
+      if (body[i].type === 'CallExpression') {
+        if (body[i].callee.type === 'MemberExpression') {
+          getRequires(requires, [body[i].callee.object])
+        }
+
+        if (body[i].callee.name === 'require') {
+          requires.push(body[i].arguments[0].value)
+        }
+      }
+    }
+      resolve(requires)
+    })
+  }
+
+  let transform
+    // function run(ast) {
+    //   console.log(ast.program.body)
+    //   for (var i = 0; i < ast.program.body.length; i++) {
+    //     console.log(ast.program.body[i])
+    //   }
+    // }
+
   console.log(`${process.cwd()}/${componentPath}`)
-  let transform = babel.transformFileSync(`${process.cwd()}/${componentPath}`, {
-    presets: ['es2015-node'],
-    plugins: [
-      'transform-react-jsx'
-    ]
+  transform = babel.transformFileSync(`${process.cwd()}/${componentPath}`, {
+      presets: ['es2015-node'],
+      plugins: [
+        'transform-react-jsx'
+      ]
+    })
+    // run(transform.ast)
+
+  getRequires([], transform.ast.program.body).then((result) => {
+    console.log(result)
+    transform = transform.code.replace('exports.default', 'module.exports')
+    console.log(transform)
+    const App = react.createElement(requireFromString(transform))
+    console.log(reactDOMServer.renderToString(App))
   })
-  transform = transform.code.replace('exports.default', 'module.exports')
-  console.log(transform)
-  const App = react.createElement(requireFromString(transform))
-  console.log(reactDOMServer.renderToString(App))
 }
 
 module.exports = Masamune
--- piranho.js
+++ piranho.js
@@ -1,7 +1,11 @@
 import React from 'React'
+import tab from './tab'
 
 export default function Piranho(props) {
     return (
+      <div>
         <section role="topbar" className="topbar" />
+        <tab/>
+      </div>
     )
 }
